f73264ab16cadc04801afdf4ffc9c0ea86d0a240
LihaoHuang <azxaxa@gmail.com>
2018-01-25 17:04:05 +0800

    建立migrate
    1. 建立: migrate

---
+++ database/migrations/2018_01_25_085206_create_osa_board_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOsaBoardTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('OSA_BOARD', function (Blueprint $table) {
+            $table->increments('BOARD_SN')->comment('主鍵 (自動流水號)');
+            $table->string('BOARD_KIND')->comment('留言類別');
+            $table->string('BOARD_PETNAME', 200)->comment('暱稱');
+            $table->string('BOARD_ACC', 10)->comment('帳號 (系統代入)');
+            $table->string('BOARD_EMAIL', 100)->comment('使用者自填mail');
+            $table->string('BOARD_TITLE')->comment('留言主題');
+            $table->string('BOARD_CONTENT')->comment('留言內容');
+            $table->string('BOARD_IP	', 20)->comment('留言ip (系統代入)');
+            $table->string('BOARD_PSN')->comment('父留言的主鍵 (回覆留言用)');
+            $table->string('BOARD_DEL', 1)->comment('刪除註記');
+            $table->timestamp('BOARD_TIME')->comment('留言時間 (系統代入)');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('osa_board');
+    }
+}
--- database/migrations/2018_01_25_085349_create_osa_boardkind_table.php
+++ database/migrations/2018_01_25_085349_create_osa_boardkind_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateOsaBoardkindTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('OSA_BOARDKIND', function (Blueprint $table) {
+            $table->increments('KIND_SN')->comment('主鍵 (自動流水號)');
+            $table->string('KIND_DESC', 200)->comment('留言類別名稱');
         });
     }
 
@@ -27,6 +26,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('OSA_BOARDKIND');
     }
 }
